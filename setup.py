2f9a245c0047ed89f75f0162ac659d11645c06e8
Svetlana Rudova <rudovasvetlana570@gmail.com>
2025-03-11 18:53:22 +0800

    commit

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(
-   name='djikstra_lib000',
-   version='1.1',
-   description='about djikstra. github: https://github.com/rudessa/djikstra_lib',
+   name='djikstra_liben',
+   version='1.0',
+   description='about djikstra.',
+   long_description=' github: https://github.com/rudessa/djikstra_lib',
    license='MIT',
    author='Svetlana Rudova',
    author_email='rudovasvetlana570@gmail.com',
